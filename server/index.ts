55539c6f3d11a26fc625f83021932ecc24133964
jeevadharan G-24BAD046 <jeevadharan24@gmailcom>
2026-09-02 21:27:41 +0530

    Fix Vercel Prisma API

--- server/index.ts
+++ server/index.ts
@@ -222,7 +222,7 @@ export default app;
 if (process.env.NODE_ENV !== "production") {
   app.listen(Number(process.env.PORT || 3000), "0.0.0.0", () => {
     console.log(
-      `Vela Store server running on http://localhost:${process.env.PORT || 3000}`
+      `Pretty Picks server running on http://localhost:${process.env.PORT || 3000}`
     );
   });
 }
